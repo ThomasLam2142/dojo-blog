07b5730e9e2bdda5d6b690f35a2558c6c3eb6e00
Thomas Lam <thomaslam2142@gmail.com>
2023-10-02 19:43:11 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import Navbar from './Navbar';
+import Home from './Home';
+
+function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  
+
+
+  return (
+    <div className='App'>
+      <Navbar />
+      
+      <div className="content">
+        <Home />
+      </div>
+
+    </div>
+  )
+}
+
+export default App
