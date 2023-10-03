abc6b95ca92aff05c049454c1df6195e2f8f6916
Thomas Lam <thomaslam2142@gmail.com>
2023-10-03 19:05:33 -0400

    added useState example

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,19 +1,22 @@
+import { useState } from 'react';
+
 const Home = () => {
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
 
-    const handleClick = (e) => {
-        console.log('hello, ninjas',e);
+
+    const handleClick = () => {
+        setName('luigi');
+        setAge(30);
     }
 
-    const handleClickAgain = (name,e) =>{
-        console.log('hello',name,e);
-    
-    }
 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click Me</button>
-            <button onClick={(e) => handleClickAgain('mario', e)}>Click Again</button>
+            
         </div>
      );
 }
